a6446ad887c99801dc85cf78734e078ddf1fa581
TaoYen <83905056+TaoYen@users.noreply.github.com>
2021-06-18 23:51:08 +0800

    Add files via upload

---
+++ cp.bat
@@ -0,0 +1,6 @@
+copy *.* E:\xampp\tomcat\webapps\brian2
+copy css\*.* E:\xampp\tomcat\webapps\brian2\css
+copy buy\*.* E:\xampp\tomcat\webapps\brian2\buy
+copy image\*.* E:\xampp\tomcat\webapps\brian2\image
+
+
